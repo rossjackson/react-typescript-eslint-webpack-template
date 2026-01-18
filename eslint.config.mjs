b4f43b4767751f6f8c066e9f238ece77edd4903b
Ross Jackson <ross.santos@gmail.com>
2026-01-18 14:40:04 -0500

    Make hot reload work

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,58 +1,64 @@
-import js from '@eslint/js';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import typescriptParser from '@typescript-eslint/parser';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import globals from 'globals';
+import js from '@eslint/js'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals'
 
 export default [
-    js.configs.recommended,
-    {
-        files: ['**/*.{js,jsx,ts,tsx}'],
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-            parser: typescriptParser,
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-            },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
-        plugins: {
-            '@typescript-eslint': typescriptEslint,
-            'react': react,
-            'react-hooks': reactHooks,
-        },
-        settings: {
-            react: {
-                pragma: 'React',
-                version: 'detect',
-            },
-        },
-        rules: {
-            ...typescriptEslint.configs.recommended.rules,
-            ...react.configs.recommended.rules,
-            ...react.configs['jsx-runtime'].rules,
-            'react/react-in-jsx-scope': 'off',
-            'react-hooks/rules-of-hooks': 'error',
-            'react-hooks/exhaustive-deps': 'warn',
-            'react/prop-types': 'off',
-            '@typescript-eslint/no-unused-vars': [
-                'error',
-                {
-                    argsIgnorePattern: '^_',
-                    varsIgnorePattern: '^_',
-                    caughtErrorsIgnorePattern: '^_',
-                },
-            ],
-        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
-    {
-        ignores: ['node_modules/**', 'dist/**', 'build/**', '*.config.js', '*.config.ts'],
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      react: react,
+      'react-hooks': reactHooks,
     },
-];
+    settings: {
+      react: {
+        pragma: 'React',
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      'build/**',
+      '*.config.js',
+      '*.config.ts',
+    ],
+  },
+]
--- webpackConfigs/webpack.prod.config.ts
+++ webpackConfigs/webpack.prod.config.ts
@@ -1,34 +1,38 @@
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import ESLintPlugin from 'eslint-webpack-plugin'
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 import path from 'path'
 import merge from 'webpack-merge'
 import sharedConfig, { Configuration } from './webpack.shared.config'
 
 const devConfig: Configuration = merge(sharedConfig, {
-    mode: 'production',
-    output: {
-        path: path.resolve(__dirname, '../build'),
-        filename: '[name].[contenthash].js',
-        publicPath: '',
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: '[name].[contenthash].js',
+    publicPath: '',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
         },
+      },
     },
-    plugins: [
-        new ForkTsCheckerWebpackPlugin({
-            async: false,
-        }),
-        new CleanWebpackPlugin(),
-    ],
+  },
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new CleanWebpackPlugin(),
+  ],
 })
 
 export default devConfig
